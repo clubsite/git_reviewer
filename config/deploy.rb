26bd3d5f327dae029a28ed095c9087b4975a807c
rjk <redmar@creativecode.nl>
2012-06-02 11:51:33 +0200

    bundle pack

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+require "bundler/capistrano"
+
+set :application, "git_reviewer"
+set :user, "rjk"
+
+set :use_sudo, false
+
+set :deploy_to, "~/#{application}"
+set :repository,  "git@github.com:clubsite/git_reviewer.git"
+set :scm, :git
+default_run_options[:pty] = true
+set :keep_releases, 5
+set :deploy_via, :remote_cache
+
+role :web, "reviews.creativecode.nl"                   
+role :app, "reviews.creativecode.nl"                   
+role :db,  "reviews.creativecode.nl", :primary => true 
+
+after "deploy:restart", "deploy:cleanup"
+
+namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
