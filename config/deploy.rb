cc196c37093f9cbb91683eb8045deebe2e37413e
rjk <redmar@creativecode.nl>
2012-06-02 12:47:38 +0200

    fixed deploy hook name for create_symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ role :app, "reviews.creativecode.nl"
 role :db,  "reviews.creativecode.nl", :primary => true 
 
 after "deploy:restart", "deploy:cleanup"
-after "deploy:symlink", "deploy:symlink_repos"
+after "deploy:create_symlink", "deploy:create_symlink_repos"
 
 namespace :deploy do
    task :start do ; end
@@ -25,7 +25,7 @@ namespace :deploy do
    task :restart, :roles => :app, :except => { :no_release => true } do
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
-   task :symlink_repos do
+   task :create_symlink_repos do
       run "mkdir -p #{shared_path}/repos && ln -nfs #{shared_path}/repos #{current_path}/repos"
    end
 end
